8af1825d4ff30ee71d11a8fd6c0283f7a372fd77
medgoh <61539610+medgoh@users.noreply.github.com>
2024-02-03 18:53:18 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+input.onButtonPressed(Button.A, function () {
+    record.setSampleRate(1000)
+    record.startRecording(record.BlockingState.Blocking)
+})
+input.onButtonPressed(Button.B, function () {
+    record.setSampleRate(22000)
+    record.playAudio(record.BlockingState.Blocking)
+})
